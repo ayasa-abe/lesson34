8baeab57c629900273456a6ed9aaad8651ff3c81
ayasa-abe <ttt36jsb@gmail.com>
2025-02-11 00:54:16 +0900

    lesson34　提出

--- src/main/java/com/techacademy/controller/EmployeeController.java
+++ src/main/java/com/techacademy/controller/EmployeeController.java
@@ -24,120 +24,133 @@ import com.techacademy.service.UserDetail;
 @RequestMapping("employees")
 public class EmployeeController {
 
-    private final EmployeeService employeeService;
+	private final EmployeeService employeeService;
 
-    @Autowired
-    public EmployeeController(EmployeeService employeeService) {
-        this.employeeService = employeeService;
-    }
+	@Autowired
+	public EmployeeController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
+	}
 
-    // 従業員一覧画面
-    @GetMapping
-    public String list(Model model) {
+	// 従業員一覧画面
+	@GetMapping
+	public String list(Model model) {
 
-        model.addAttribute("listSize", employeeService.findAll().size());
-        model.addAttribute("employeeList", employeeService.findAll());
+		model.addAttribute("listSize", employeeService.findAll().size());
+		model.addAttribute("employeeList", employeeService.findAll());
 
-        return "employees/list";
-    }
+		return "employees/list";
+	}
 
-    // 従業員詳細画面
-    @GetMapping(value = "/{code}/")
-    public String detail(@PathVariable("code") String code, Model model) {
+	// 従業員詳細画面
+	@GetMapping(value = "/{code}/")
+	public String detail(@PathVariable("code") String code, Model model) {
 
-        model.addAttribute("employee", employeeService.findByCode(code));
-        return "employees/detail";
-    }
+		model.addAttribute("employee", employeeService.findByCode(code));
+		return "employees/detail";
+	}
 
-    // 従業員新規登録画面
-    @GetMapping(value = "/add")
-    public String create(@ModelAttribute Employee employee) {
+	// 従業員新規登録画面
+	@GetMapping(value = "/add")
+	public String create(@ModelAttribute Employee employee) {
 
-        return "employees/new";
-    }
+		return "employees/new";
+	}
 
-    // 従業員新規登録処理
-    @PostMapping(value = "/add")
-    public String add(@Validated Employee employee, BindingResult res, Model model) {
+	// 従業員新規登録処理
+	@PostMapping(value = "/add")
+	public String add(@Validated Employee employee, BindingResult res, Model model) {
 
-        // パスワード空白チェック
-        /*
-         * エンティティ側の入力チェックでも実装は行えるが、更新の方でパスワードが空白でもチェックエラーを出さずに
-         * 更新出来る仕様となっているため上記を考慮した場合に別でエラーメッセージを出す方法が簡単だと判断
-         */
-        if ("".equals(employee.getPassword())) {
-            // パスワードが空白だった場合
-            model.addAttribute(ErrorMessage.getErrorName(ErrorKinds.BLANK_ERROR),
-                    ErrorMessage.getErrorValue(ErrorKinds.BLANK_ERROR));
+		// パスワード空白チェック
+		/*
+		 * エンティティ側の入力チェックでも実装は行えるが、更新の方でパスワードが空白でもチェックエラーを出さずに
+		 * 更新出来る仕様となっているため上記を考慮した場合に別でエラーメッセージを出す方法が簡単だと判断
+		 */
+		if ("".equals(employee.getPassword())) {
+			// パスワードが空白だった場合
+			model.addAttribute(ErrorMessage.getErrorName(ErrorKinds.BLANK_ERROR),
+					ErrorMessage.getErrorValue(ErrorKinds.BLANK_ERROR));
 
-            return create(employee);
+			return create(employee);
 
-        }
+		}
 
-        // 入力チェック
-        if (res.hasErrors()) {
-            return create(employee);
-        }
+		// 入力チェック
+		if (res.hasErrors()) {
+			return create(employee);
+		}
 
-        // 論理削除を行った従業員番号を指定すると例外となるためtry~catchで対応
-        // (findByIdでは削除フラグがTRUEのデータが取得出来ないため)
-        try {
-            ErrorKinds result = employeeService.save(employee);
+		// 論理削除を行った従業員番号を指定すると例外となるためtry~catchで対応
+		// (findByIdでは削除フラグがTRUEのデータが取得出来ないため)
+		try {
+			ErrorKinds result = employeeService.save(employee);
 
-            if (ErrorMessage.contains(result)) {
-                model.addAttribute(ErrorMessage.getErrorName(result), ErrorMessage.getErrorValue(result));
-                return create(employee);
-            }
+			if (ErrorMessage.contains(result)) {
+				model.addAttribute(ErrorMessage.getErrorName(result), ErrorMessage.getErrorValue(result));
+				return create(employee);
+			}
 
-        } catch (DataIntegrityViolationException e) {
-            model.addAttribute(ErrorMessage.getErrorName(ErrorKinds.DUPLICATE_EXCEPTION_ERROR),
-                    ErrorMessage.getErrorValue(ErrorKinds.DUPLICATE_EXCEPTION_ERROR));
-            return create(employee);
-        }
+		} catch (DataIntegrityViolationException e) {
+			model.addAttribute(ErrorMessage.getErrorName(ErrorKinds.DUPLICATE_EXCEPTION_ERROR),
+					ErrorMessage.getErrorValue(ErrorKinds.DUPLICATE_EXCEPTION_ERROR));
+			return create(employee);
+		}
 
-        return "redirect:/employees";
-    }
+		return "redirect:/employees";
+	}
 
-    //　従業員更新画面
-    @GetMapping(value = "/update/{code}")
-    public String edit(@PathVariable("code") String code, Model model) {
-    	Employee employee = employeeService.findByCode(code);
-        model.addAttribute("employee", employee);
+	// 従業員更新画面
+	@GetMapping(value = "/{code}/update")
+	public String edit(@PathVariable("code") String code,@ModelAttribute Employee employee, Model model) {
+		model.addAttribute("employee", employeeService.findByCode(code));
 
-        // 従業印更新画面に遷移
-        return "employees/update";
-    }
+		// 従業印更新画面に遷移
+		return "employees/update";
+	}
 
-    
-    
-    /**　従業員更新処理 */
-    @PostMapping("/update/{code}/")
-    public String update (@PathVariable("code") String code, @Validated Employee employee, BindingResult bindingResult, Model model) {
-    	if (bindingResult.hasErrors()) {
-            model.addAttribute("employee", employee);
-            return "employees/update";
-        }
+	/** 従業員更新処理 */
+	@PostMapping(value = "/{code}/update")
+	public String update(@PathVariable("code") String code, @Validated Employee employee, BindingResult res,Model model) {
+		// 入力チェック
+		if (res.hasErrors()) {
+			model.addAttribute("employee", employee);
+			return "employees/update";
+		}
 
-        employee.setCode(code);
-        employeeService.save(employee);
+		// employeeService.updateを呼び出す
+		ErrorKinds result = employeeService.update(employee);
+		// もしチェックNGだった場合（戻り値がErrorKinds.CHECK_OKじゃない場合）、従業員更新画面に戻る
+		if (ErrorMessage.contains(result)) {
+			 model.addAttribute(ErrorMessage.getErrorName(result), ErrorMessage.getErrorValue(result));
+			    return "employees/update";
+		}
+
+	    model.addAttribute("employee", employeeService.findByCode(code));
+	    if ("".equals(employee.getName())) {
+	    	//　氏名が空白だった場合
+	    	model.addAttribute(ErrorMessage.getErrorName(ErrorKinds.BLANK_ERROR),
+	    			ErrorMessage.getErrorValue(ErrorKinds.BLANK_ERROR));
+
+	    	return edit(employee.getCode(),employee,model);
+	    }
+
+		    return "redirect:/employees";
+		}
 
 
-        return "redirect:/employees";
-    }
+	// 従業員削除処理
+	@PostMapping(value = "/{code}/delete")
+	public String delete(@PathVariable("code") String code, @AuthenticationPrincipal UserDetail userDetail,
+			Model model) {
 
-    // 従業員削除処理
-    @PostMapping(value = "/{code}/delete")
-    public String delete(@PathVariable("code") String code, @AuthenticationPrincipal UserDetail userDetail, Model model) {
+		ErrorKinds result = employeeService.delete(code, userDetail);
 
-        ErrorKinds result = employeeService.delete(code, userDetail);
+		if (ErrorMessage.contains(result)) {
+			model.addAttribute(ErrorMessage.getErrorName(result), ErrorMessage.getErrorValue(result));
+			model.addAttribute("employee", employeeService.findByCode(code));
+			return detail(code, model);
+		}
 
-        if (ErrorMessage.contains(result)) {
-            model.addAttribute(ErrorMessage.getErrorName(result), ErrorMessage.getErrorValue(result));
-            model.addAttribute("employee", employeeService.findByCode(code));
-            return detail(code, model);
-        }
-
-        return "redirect:/employees";
-    }
+		return "redirect:/employees";
+	}
 
 }
--- src/main/java/com/techacademy/service/EmployeeService.java
+++ src/main/java/com/techacademy/service/EmployeeService.java
@@ -17,51 +17,46 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class EmployeeService {
 
-    private final EmployeeRepository employeeRepository;
-    private final PasswordEncoder passwordEncoder;
+	private final EmployeeRepository employeeRepository;
+	private final PasswordEncoder passwordEncoder;
 
-    public EmployeeService(EmployeeRepository employeeRepository, PasswordEncoder passwordEncoder) {
-        this.employeeRepository = employeeRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
+	public EmployeeService(EmployeeRepository employeeRepository, PasswordEncoder passwordEncoder) {
+		this.employeeRepository = employeeRepository;
+		this.passwordEncoder = passwordEncoder;
+	}
 
-    // 従業員保存
-    @Transactional
-    public ErrorKinds save(Employee employee) {
+	// 従業員保存
+	@Transactional
+	public ErrorKinds save(Employee employee) {
 
-        // パスワードチェック
-        ErrorKinds result = employeePasswordCheck(employee);
-        if (ErrorKinds.CHECK_OK != result) {
-            return result;
-        }
+		// パスワードチェック
+		ErrorKinds result = employeePasswordCheck(employee);
+		if (ErrorKinds.CHECK_OK != result) {
+			return result;
+		}
 
-        // 従業員番号重複チェック
-        if (findByCode(employee.getCode()) != null) {
-            return ErrorKinds.DUPLICATE_ERROR;
-        }
+		// 従業員番号重複チェック
+		if (findByCode(employee.getCode()) != null) {
+			return ErrorKinds.DUPLICATE_ERROR;
+		}
 
-        employee.setDeleteFlg(false);
+		employee.setDeleteFlg(false);
 
-        LocalDateTime now = LocalDateTime.now();
-        employee.setCreatedAt(now);
-        employee.setUpdatedAt(now);
+		LocalDateTime now = LocalDateTime.now();
+		employee.setCreatedAt(now);
+		employee.setUpdatedAt(now);
 
-        employeeRepository.save(employee);
-        return ErrorKinds.SUCCESS;
-    }
+		employeeRepository.save(employee);
+		return ErrorKinds.SUCCESS;
+	}
 
+	// 従業員更新
+	@Transactional
+    public ErrorKinds update(Employee employee) {
+		Employee emp = findByCode(employee.getCode());
+		if("".equals(employee.getPassword())) {
+			employee.setPassword(emp.getPassword());
 
-    //　従業員更新
-    @Transactional
-    public ErrorKinds update(Employee employee, String code, String password, String role ) {
-
-    	// 社員情報を取得(findByCodeメソッドを利用)
-    	Employee motoemployee = findByCode(code);
-
-    	// パスワードの入力欄が空の場合
-    	if (password == null || password.isEmpty()) {
-    	// 登録済のパスワードをそのまま使用
-    	   password = motoemployee.getPassword();
     	} else {
 
     	    // パスワードの入力欄が空でない場合
@@ -72,102 +67,81 @@ public class EmployeeService {
                 return result;
             }
         }
-    	// employeeオブジェクトの必要な情報を設定していく
-    	motoemployee.setName(employee.getName());  // 新しい名前に更新
-    	if (password != null && !password.isEmpty()) {
-    	    motoemployee.setPassword(password);  // 新しいパスワードに更新
-    	    }
-    	 // 権限の設定（プルダウンで選択された権限）
-        if (role != null && !role.isEmpty()) {
-            // String 型を Role 列挙型に変換
-            Employee.Role newRole;
-            if ("一般".equals(role)) {
-                newRole = Employee.Role.GENERAL;
-            } else if ("管理者".equals(role)) {
-                newRole = Employee.Role.ADMIN;
-            } else {
-                // 無効な値が渡された場合はデフォルトの権限を設定
-                newRole = Employee.Role.GENERAL;
-            }
-            motoemployee.setRole(newRole);  // "一般" または "管理者" の選択された権限を設定
-        }
+
+		employee.setDeleteFlg(false);
+
     	LocalDateTime now = LocalDateTime.now();  // 現在の日時を取得
-        employee.setUpdatedAt(now);  // 従業員の更新日時を設定
+        employee.setCreatedAt(emp.getCreatedAt());
 
         employeeRepository.save(employee);  // 従業員オブジェクトをデータベースに保存
-
-    	// employeeRepository.(employee)で更新
-    	employeeRepository.save(motoemployee);  // 更新された社員情報を保存
-
-        return ErrorKinds.CHECK_OK;  // 更新成功
-    	 }
-
-
-    // 従業員削除
-    @Transactional
-    public ErrorKinds delete(String code, UserDetail userDetail) {
-
-        // 自分を削除しようとした場合はエラーメッセージを表示
-        if (code.equals(userDetail.getEmployee().getCode())) {
-            return ErrorKinds.LOGINCHECK_ERROR;
-        }
-        Employee employee = findByCode(code);
-        LocalDateTime now = LocalDateTime.now();
-        employee.setUpdatedAt(now);
-        employee.setDeleteFlg(true);
-
-        return ErrorKinds.SUCCESS;
-    }
-
-    // 従業員一覧表示処理
-    public List<Employee> findAll() {
-        return employeeRepository.findAll();
-    }
-
-    // 1件を検索
-    public Employee findByCode(String code) {
-        // findByIdで検索
-        Optional<Employee> option = employeeRepository.findById(code);
-        // 取得できなかった場合はnullを返す
-        Employee employee = option.orElse(null);
-        return employee;
-    }
-
-    // 従業員パスワードチェック
-    private ErrorKinds employeePasswordCheck(Employee employee) {
-
-        // 従業員パスワードの半角英数字チェック処理
-        if (isHalfSizeCheckError(employee)) {
-
-            return ErrorKinds.HALFSIZE_ERROR;
+        return ErrorKinds.SUCCESS; // 更新成功
         }
 
-        // 従業員パスワードの8文字～16文字チェック処理
-        if (isOutOfRangePassword(employee)) {
+	// 従業員削除
+	@Transactional
+	public ErrorKinds delete(String code, UserDetail userDetail) {
 
-            return ErrorKinds.RANGECHECK_ERROR;
-        }
+		// 自分を削除しようとした場合はエラーメッセージを表示
+		if (code.equals(userDetail.getEmployee().getCode())) {
+			return ErrorKinds.LOGINCHECK_ERROR;
+		}
+		Employee employee = findByCode(code);
+		LocalDateTime now = LocalDateTime.now();
+		employee.setUpdatedAt(now);
+		employee.setDeleteFlg(true);
 
-        employee.setPassword(passwordEncoder.encode(employee.getPassword()));
+		return ErrorKinds.SUCCESS;
+	}
 
-        return ErrorKinds.CHECK_OK;
-    }
+	// 従業員一覧表示処理
+	public List<Employee> findAll() {
+		return employeeRepository.findAll();
+	}
 
-    // 従業員パスワードの半角英数字チェック処理
-    private boolean isHalfSizeCheckError(Employee employee) {
+	// 1件を検索
+	public Employee findByCode(String code) {
+		// findByIdで検索
+		Optional<Employee> option = employeeRepository.findById(code);
+		// 取得できなかった場合はnullを返す
+		Employee employee = option.orElse(null);
+		return employee;
+	}
 
-        // 半角英数字チェック
-        Pattern pattern = Pattern.compile("^[A-Za-z0-9]+$");
-        Matcher matcher = pattern.matcher(employee.getPassword());
-        return !matcher.matches();
-    }
+	// 従業員パスワードチェック
+	private ErrorKinds employeePasswordCheck(Employee employee) {
 
-    // 従業員パスワードの8文字～16文字チェック処理
-    public boolean isOutOfRangePassword(Employee employee) {
+		// 従業員パスワードの半角英数字チェック処理
+		if (isHalfSizeCheckError(employee)) {
 
-        // 桁数チェック
-        int passwordLength = employee.getPassword().length();
-        return passwordLength < 8 || 16 < passwordLength;
-    }
+			return ErrorKinds.HALFSIZE_ERROR;
+		}
+
+		// 従業員パスワードの8文字～16文字チェック処理
+		if (isOutOfRangePassword(employee)) {
+
+			return ErrorKinds.RANGECHECK_ERROR;
+		}
+
+		employee.setPassword(passwordEncoder.encode(employee.getPassword()));
+
+		return ErrorKinds.CHECK_OK;
+	}
+
+	// 従業員パスワードの半角英数字チェック処理
+	private boolean isHalfSizeCheckError(Employee employee) {
+
+		// 半角英数字チェック
+		Pattern pattern = Pattern.compile("^[A-Za-z0-9]+$");
+		Matcher matcher = pattern.matcher(employee.getPassword());
+		return !matcher.matches();
+	}
+
+	// 従業員パスワードの8文字～16文字チェック処理
+	public boolean isOutOfRangePassword(Employee employee) {
+
+		// 桁数チェック
+		int passwordLength = employee.getPassword().length();
+		return passwordLength < 8 || 16 < passwordLength;
+	}
 
 }
